3b6544355b2252217cd3cf4f2098cb1e3770b57b
tealun <tealun@gmail.com>
2014-03-26 22:13:35 +0800

    自定义配置更改
    删除站点域名配置，因为可以用$_SERVER['HTTP_HOST']获取；
    新增账号性质以及认证状态配置

--- Application/Wechat/Conf/config.php
+++ Application/Wechat/Conf/config.php
@@ -6,7 +6,8 @@ define('TCHAT_PHOTO','./Uploads/Wechat/Tchat/Picture/');
 define('TCHAT_VOICE','./Uploads/Wechat/Tchat/Voice/');
 define('TCHAT_VIDEO','./Uploads/Wechat/Tchat/Video/');
 
-define('WEB_HOST_NAME', 'tchat2.guilingo.com');
+define('WEB_ACCOUNT_TYPE', '0');//账号性质 0订阅号 1服务号；
+define('WEB_ACCOUNT_RZ', '0');//是否认证 0未认证 1已认证 2微博认证
 define('WECHAT_APP_ID', 'wxcb33ed4ad496e156');
 define('WECHAT_APP_SECRET', 'cc23bb31ae3921eef4af82dba3e23bb6');
 
