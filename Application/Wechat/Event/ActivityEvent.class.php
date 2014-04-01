a91ca1940952ad1a667c34a313b310d6aafc2f72
tealun <tealun@gmail.com>
2014-04-01 13:50:59 +0800

    注释方法

--- Application/Wechat/Event/ActivityEvent.class.php
+++ Application/Wechat/Event/ActivityEvent.class.php
@@ -10,16 +10,22 @@ namespace Wechat\Event;
 
 class ActivityEvent{
 
-	//TODO 对特定活动的关键词进行过滤匹配
+	/**
+	 * 对特定活动板块进行检测
+	 * 
+	 * @param $openId 微信openid
+	 * @param $keyword 客户发送的信息
+	 */
 	public function activPreg($openId,$keyword){
-		$patternArr = array(
+		$patternArr = array( // 建立特定板块关键词匹配规则
 			'hongbao'=>"/^(?:抢红包)[ \-\+]?([^ \+\f\n\r\t\v0-9]+)?[ \-\+]?((1[34578]\d{9})$|((0\d{2,3})?-?(\d{7,8}))$)?/",
-			'suggest'=>"/^(?:建议)[ \-\+\:]?(.*)/"
+			'suggest'=>"/^(?:建议|提建议)[ \-\+\:]?(.*)/"
 		);
+		//对客户发送的消息进行关键词匹配
 		foreach ($patternArr as $key => $pattern){
 			if(preg_match($pattern, $keyword,$matches)){
 				switch ($key){
-					//红包活动
+					//匹配到红包活动
 					case 'hongbao':
 						//先查看活动是否已经截止或者禁用及删除
 						$map['segment'] = 'activity';
@@ -28,6 +34,7 @@ class ActivityEvent{
 						$map['deadline'] = array('NOT BETWEEN',array('1',time()));
 						$id = M('Tchat_keyword_group')->where($map)->getField('id');
 						
+						//没有结果时，回复结束时的回复内容
 						if(!$id){
 							$where['segment'] = 'activity';
 							$where['name'] = '抢红包';
@@ -36,12 +43,14 @@ class ActivityEvent{
 							exit;
 						}
 						
-						S($openId,NULL); //如果匹配，先清除客户的缓存
-						$name = $matches[1];
-						$phone = $matches[2];
+						//如果匹配且有结果，先清除客户的缓存
+						S($openId,NULL); 
+						$name = $matches[1]; //提取客户姓名
+						$phone = $matches[2]; //提取客户电话
 						if(!$name){$tip[name]='姓名';}
 						if(!$phone){$tip[phone]='联系电话';}
-
+						
+						//提取不到姓名或者电话时新增客户缓存，并提示需要回复哪些内容
 						if(!$name || !$phone){
 							$tips = implode('、',$tip);
 							$content = "您的".$tips."输入有误，请重新回复您的".$tips."\n（2分钟内有效）";
@@ -64,14 +73,14 @@ class ActivityEvent{
 							return $reply = A('Reply','Event')->wechatReply($rs);
 						}
 						break;
-					//客户建议
+					//匹配到客户建议
 					case 'suggest':
-						
+						//获取客户ID并存储客户建议内容
 						$data['client_id']=D('Tchat_client')->getClintId($openId);
 						$data['content'] = $matches[1];
 						$data['create_time'] = time();
 						M('Tchat_suggestion')->data($data)->add();
-
+						//提示客户回复姓名和电话，并建立客户缓存
 						$reply = get_text_arr("您的建议我们已经收到，希望您能回复您的姓名和联系方式，如有必要我们会联系您。谢谢！\n (2分钟后过期)");
 						S($openId,array('activity'=>'suggest'),120);
 				}
