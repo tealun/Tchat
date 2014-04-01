b9e4330d09fa8ea141984db0c2fc71456f88a13b
tealun <tealun@gmail.com>
2014-04-01 17:41:28 +0800

    删除配置文件中APPID和APPSECRET定义项

--- Application/Wechat/Conf/config.php
+++ Application/Wechat/Conf/config.php
@@ -8,6 +8,3 @@ define('TCHAT_VIDEO','./Uploads/Wechat/Tchat/Video/');
 
 define('WEB_ACCOUNT_TYPE', '0');//账号性质 0订阅号 1服务号；
 define('WEB_ACCOUNT_RZ', '0');//是否认证 0未认证 1已认证 2微博认证
-define('WECHAT_APP_ID', 'wxcb33ed4ad496e156');
-define('WECHAT_APP_SECRET', 'cc23bb31ae3921eef4af82dba3e23bb6');
-
