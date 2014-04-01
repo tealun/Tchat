39dc24104f3650f4bf2d58af72e6fbbda78006f5
tealun <tealun@gmail.com>
2014-04-01 22:11:13 +0800

    删除config文件中的配置项

--- Application/Wechat/Conf/config.php
+++ Application/Wechat/Conf/config.php
@@ -5,6 +5,3 @@ define('CLIENT_VIDEO','./Uploads/Wechat/Client/Video/');
 define('TCHAT_PHOTO','./Uploads/Wechat/Tchat/Picture/');
 define('TCHAT_VOICE','./Uploads/Wechat/Tchat/Voice/');
 define('TCHAT_VIDEO','./Uploads/Wechat/Tchat/Video/');
-
-define('WEB_ACCOUNT_TYPE', '0');//账号性质 0订阅号 1服务号；
-define('WEB_ACCOUNT_RZ', '0');//是否认证 0未认证 1已认证 2微博认证
