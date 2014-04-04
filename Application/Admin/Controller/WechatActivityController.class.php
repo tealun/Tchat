e5bbd1a9577473db7300f1e51143c5c4592db081
tealun <tealun@gmail.com>
2014-04-04 21:51:23 +0800

    Revert "针对活动板块做的开发"
    This reverts commit c731227f59b393c0f894a3a647cb5c62dbb3b1ca.

--- Application/Admin/Controller/WechatActivityController.class.php
+++ Application/Admin/Controller/WechatActivityController.class.php
@@ -16,56 +16,8 @@ namespace Admin\Controller;
 class WechatActivityController extends WechatController {
 
 	public function index(){
-    $map=$this->indexOfMap();
-    $map['deadline'] = array('not between',array(1,time()));
-    $this->getLists($map);
-    $this->meta_title = '关键词列表';
-    $this->display(); // 输出模板
+		$this->display();
 	}
 
-      /**
-     * 查询条件初始化
-     *
-     */
-    protected function indexOfMap(){
-        /* 查询条件初始化 */
-        $map = array();
-        if(isset($_GET['actType'])){
-            $map['act_type']  = (string)$_GET['actType'];
-            $this->assign('actType', $map['act_type']);
-        }
-        if(isset($_GET['keyword'])){
-            $map['name']  = array('like', '%'.(string)I('keyword').'%');
-            $this->assign('keyword', $_GET['keyword']);
-        }
-        if(isset($_GET['status'])){
-            $map['status'] = I('status');
-            $status = $map['status'];
-        }else{
-            $status = null;
-      $map['status'] = array('egt',0);
-        }
 
-        if ( isset($_GET['time-start']) ) {
-            $map['update_time'][] = array('egt',strtotime(I('time-start')));
-        }
-        if ( isset($_GET['time-end']) ) {
-            $map['update_time'][] = array('elt',24*60*60 + strtotime(I('time-end')));
-        }
-        if ( isset($_GET['nickname']) ) {
-            $map['uid'] = M('Member')->where(array('nickname'=>I('nickname')))->getField('uid');
-        }
-    return $map;
-    }
-    
-    /**
-     * 获取列表
-     * 
-     * @param array $map
-     */
-    private function getLists($map){
-      $list   = $this->lists('Tchat_activity',$map);
-      col_to_string($list);
-    $this->assign('_list', $list);
-    }
 }
