25ed10d6122f22e21739a5004f130032b12f7f50
tealun <tealun@gmail.com>
2014-08-10 23:30:29 +0800

    edit these function in WechatMenu class , like 'edit','add','update'...

--- Application/Admin/Controller/WechatMenuController.class.php
+++ Application/Admin/Controller/WechatMenuController.class.php
@@ -60,62 +60,107 @@ class WechatMenuController extends WechatController {
 
 	/* 编辑菜单 */
 	public function edit($id = null, $pid = 0) {
-		$TchatMenu = D('Tchat_menu');
 
-		if (IS_POST) {//提交表单
-			if (false !== $TchatMenu -> update()) {
-				$this -> success('编辑成功！', U('viewMenu'));
-			} else {
-				$error = $TchatMenu -> getError();
-				$this -> error(empty($error) ? '未知错误！' : $error);
-			}
-		} else {
-			$menu = '';
+		if (IS_GET) {
+		   $id    =   I('get.id','');
+	        if(empty($id)){
+	            $this->error('参数不能为空！');
+	        }
+			
+			$info['model_id']= '52';
+			$info['id'] = $id;
+			$info['pid'] = $pid;
+      
+      /*获取一条记录的详细数据*/
+        $TchatMenu = D('Tchat_menu'); 
+        $data = $TchatMenu->info($id);
+        if(!$data){
+            $this->error($TchatMenu->getError());
+        }
+		
+		$data = json_encode($data);
+		
+        $this->assign('data',$data);
+      
+        //获取菜单模型
+        $model = M('Model')->where(array('id'=>$info['model_id']))->find();
+    
+            //获取表单字段排序
+        $fields = get_model_attribute($model['id']);
+		$this -> assign('info', $info);
+        $this->assign('fields',     $fields);
+        $this->assign('model', $model);
+			
+		}
+			//如果有传入上级菜单ID，判断上级菜单的状态并且获取相关信息
 			if ($pid) {
 				/* 获取上级菜单信息 */
-				$menu = $TchatMenu -> info($pid, 'id,name,status');
-				if (!($menu && 1 == $menu['status'])) {
+				$pidInfo = $TchatMenu -> info($pid, 'id,name,status');
+				if (!($pidInfo && 1 == $pidInfo['status'])) {
 					$this -> error('指定的上级菜单不存在或被禁用！');
 				}
+				//赋值上级菜单信息
+				$this -> assign('pidInfo', $pidInfo);
 			}
-
-			/* 获取菜单信息 */
-			$info = $id ? $TchatMenu -> info($id) : '';
-
-			$this -> assign('info', $info);
-			$this -> assign('menu', $menu);
+			
 			$this -> meta_title = '编辑菜单';
 			$this -> display();
-		}
+
 	}
 
-	/* 新增菜单 */
+	/**
+	 * 新增菜单
+	 */
 	public function add($pid = 0) {
 		$TchatMenu = D('Tchat_menu');
+		$info['model_id']='52';
+		if ($pid) {
+				/* 获取上级菜单信息 */
+				$pidInfo = $TchatMenu -> info($pid, 'id,name,status');
+				if (!($pidInfo && 1 == $pidInfo['status'])) {
+					$this -> error('指定的上级菜单不存在或被禁用！');
+				}else{
+					$this -> assign('pidInfo',$pidInfo); //获取到的上级菜单信息，用于添加二级菜单时的数据
+				}
+			}
+        /* 新增菜单页面的变量赋值 */
+        
+        //获取菜单模型
+        $model = M('Model')->where(array('id'=>$info['model_id']))->find();
+    
+            //获取表单字段排序
+        $fields = get_model_attribute($model['id']);
+		$this -> assign('info', $info);
+        $this->assign('fields',     $fields);
+        $this->assign('model', $model);
+			/* 获取菜单信息 */
+			$this -> meta_title = '新增菜单';
+			$this -> display('edit');
 
-		if (IS_POST) {//提交表单
+	}
+
+/**
+ * 新增或更新数据
+ */
+public function update(){
+			
+			$TchatMenu = D('Tchat_menu');
+			if (IS_POST) {
+				$id = I('post.id');
 			if (false !== $TchatMenu -> update()) {
-				$this -> success('新增成功！', U('viewMenu'));
+				if(!empty($id)){
+						$this -> success('更新成功！', U('viewMenu'));
+				}else{
+						$this -> success('新增成功！', U('viewMenu'));
+				}
+
 			} else {
 				$error = $TchatMenu -> getError();
 				$this -> error(empty($error) ? '未知错误！' : $error);
 			}
-		} else {
-			$menu = array();
-			if ($pid) {
-				/* 获取上级菜单信息 */
-				$menu = $TchatMenu -> info($pid, 'id,name,status');
-				if (!($menu && 1 == $menu['status'])) {
-					$this -> error('指定的上级菜单不存在或被禁用！');
-				}
-			}
-
-			/* 获取菜单信息 */
-			$this -> assign('menu', $menu);
-			$this -> meta_title = '新增菜单';
-			$this -> display('edit');
-		}
-	}
+		} 
+	
+}
 
 	/**
 	 * 删除一个菜单
@@ -137,7 +182,7 @@ class WechatMenuController extends WechatController {
 		$res = M('Tchat_menu') -> delete($menu_id);
 		if ($res !== false) {
 			//记录行为
-			action_log('update_tchat_menu', 'tchatMenu', $menu_id, UID);
+			action_log('delete_tchat_menu', 'tchatMenu', $menu_id, UID);
 			$this -> success('删除菜单成功！');
 		} else {
 			$this -> error('删除菜单失败！');
@@ -204,22 +249,19 @@ class WechatMenuController extends WechatController {
 		//if (IS_POST) {
 		$tree = D('Tchat_menu') -> getTree(0, 'id,pid,type,name,key,url');
 
-		$tree = $this -> clearTreeArr($tree);
-		$menu['button'] = $tree;
+		$menu['button'] = $this -> clearTreeArr($tree); //清理掉$tree中用于排序和分级而获取到的字段，这些字段不需要发送到服务器
+		$content = json_encode($menu); //使用PHP的json_decode()函数将数组转换为JSON格式，但汉字会变成 \uxxxx形式的问题
+        $content = decodeUnicode($content); //使用一个函数解决转换过来的JSON数据中汉字变成 \uxxxx形式的问题
 
-		$content = json_encode($menu, true);
-
-//		if ($this -> deleteMenu()) {
 			//POST提交地址
 			$url = 'https://api.weixin.qq.com/cgi-bin/menu/create?access_token=' . get_access_token();
 			$rs = vpost($url, $content);
 			if ($rs) {
-				S('WECHAT_MENU', $data);
+				S('WECHAT_MENU', $content);
 				$this -> success('更新成功！');
 			} else {
 				$this -> error('更新失败！');
 			}
-//		}
 
 	}
 
@@ -241,6 +283,7 @@ class WechatMenuController extends WechatController {
 	private function clearTreeArr($arr) {
 		if (is_array($arr)) {
 			foreach ($arr as $key => $value) {
+	
 				//删除不必要的ID PID选项
 				unset($arr[$key]['id'], $arr[$key]['pid']);
 
