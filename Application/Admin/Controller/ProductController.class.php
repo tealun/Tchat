d4c56e556ded909cee453a61fc19e537d7b1ad0d
tealun <tealun@gmail.com>
2014-08-28 18:00:05 +0800

    Edit ProductController, add new index HTML file of ProductController.

--- Application/Admin/Controller/ProductController.class.php
+++ Application/Admin/Controller/ProductController.class.php
@@ -8,21 +8,167 @@
 // +----------------------------------------------------------------------
 
 namespace Admin\Controller;
-
+use Admin\Model\AuthGroupModel;
+use Think\Page;
 /**
  *相册板块后台管理控制器
  * 
  */
-class ProductController extends AdminController {
+class ProductController extends ArticleController {
 
-	public function index(){
-		
-		$hostname = $_SERVER["HTTP_HOST"].C('WECHAT_APP_ID');
-		$this->assign('hostname',$hostname);
-		$this->display();
-	}
+    public function index($cate_id = null){
+        //获取左边菜单
+        $this->getMenu();
 
+        if($cate_id===null){
+            $cate_id = $this->cate_id;
+        }
 
+        //获取模型信息
+        $model = M('Model')->getByName('product');
 
+        //解析列表规则
+        $fields = array();
+        $grids  = preg_split('/[;\r\n]+/s', $model['list_grid']);
+        foreach ($grids as &$value) {
+            // 字段:标题:链接
+            $val      = explode(':', $value);
+            // 支持多个字段显示
+            $field   = explode(',', $val[0]);
+            $value    = array('field' => $field, 'title' => $val[1]);
+            if(isset($val[2])){
+                // 链接信息
+                $value['href']  =   $val[2];
+                // 搜索链接信息中的字段信息
+                preg_replace_callback('/\[([a-z_]+)\]/', function($match) use(&$fields){$fields[]=$match[1];}, $value['href']);
+            }
+            if(strpos($val[1],'|')){
+                // 显示格式定义
+                list($value['title'],$value['format'])    =   explode('|',$val[1]);
+            }
+            foreach($field as $val){
+                $array  =   explode('|',$val);
+                $fields[] = $array[0];
+            }
+        }
+
+        // 过滤重复字段信息 TODO: 传入到查询方法
+        $fields = array_unique($fields);
+
+        //获取对应分类下的模型
+        if(!empty($cate_id)){   //没有权限则不查询数据
+            //获取分类绑定的模型
+            $models         =   get_category($cate_id, 'model');
+            $allow_reply    =   get_category($cate_id, 'reply');//分类文档允许回复
+            $pid            =   I('pid');
+            if ( $pid==0 ) {
+                //开发者可根据分类绑定的模型,按需定制分类文档列表
+                $template = $this->indexOfArticle( $cate_id ); //转入默认文档列表方法
+                $this->assign('model',  explode(',',$models));
+            }else{
+                //开发者可根据父文档的模型类型,按需定制子文档列表
+                $doc_model = M('Document')->where(array('id'=>$pid))->find();
+
+                switch($doc_model['model_id']){
+                    default:
+                        if($doc_model['type']==2 && $allow_reply){
+                            $this->assign('model',  array(2));
+                            $template = $this->indexOfReply( $cate_id ); //转入子文档列表方法
+                        }else{
+                            $this->assign('model',  explode(',',$models));
+                            $template = $this->indexOfArticle( $cate_id ); //转入默认文档列表方法
+                        }
+                }
+            }
+
+            $this->assign('list_grids', $grids);
+            $this->assign('model_list', $model);
+            // 记录当前列表页的cookie
+            Cookie('__forward__',$_SERVER['REQUEST_URI']);
+            $this->display($template);
+        }else{
+            $this->error('非法的文档分类');
+        }
+    }
+
+    /**
+     * 显示左边菜单，进行权限控制
+     * @author huajie <banhuajie@163.com>
+     */
+    protected function getMenu(){
+        //获取动态分类
+        $cate_auth  =   AuthGroupModel::getAuthCategories(UID);	//获取当前用户所有的内容权限节点
+        $cate_auth  =   $cate_auth == null ? array() : $cate_auth;
+        $cate       =   M('Category')->where(array('status'=>1,'model_id'=>53))->field('id,title,pid,allow_publish')->order('pid,sort')->select();
+
+        //没有权限的分类则不显示
+        if(!IS_ROOT){
+            foreach ($cate as $key=>$value){
+                if(!in_array($value['id'], $cate_auth)){
+                    unset($cate[$key]);
+                }
+            }
+        }
+
+        $cate           =   list_to_tree($cate);	//生成分类树
+
+        //获取分类id
+        $cate_id        =   I('param.cate_id');
+        $this->cate_id  =   $cate_id;
+
+        //是否展开分类
+        $hide_cate = false;
+        if(ACTION_NAME != 'recycle' && ACTION_NAME != 'draftbox' && ACTION_NAME != 'mydocument'){
+            $hide_cate  =   true;
+        }
+
+        //生成每个分类的url
+        foreach ($cate as $key=>&$value){
+            $value['url']   =   'Article/index?cate_id='.$value['id'];
+            if($cate_id == $value['id'] && $hide_cate){
+                $value['current'] = true;
+            }else{
+                $value['current'] = false;
+            }
+            if(!empty($value['_child'])){
+                $is_child = false;
+                foreach ($value['_child'] as $ka=>&$va){
+                    $va['url']      =   'Article/index?cate_id='.$va['id'];
+                    if(!empty($va['_child'])){
+                        foreach ($va['_child'] as $k=>&$v){
+                            $v['url']   =   'Article/index?cate_id='.$v['id'];
+                            $v['pid']   =   $va['id'];
+                            $is_child = $v['id'] == $cate_id ? true : false;
+                        }
+                    }
+                    //展开子分类的父分类
+                    if($va['id'] == $cate_id || $is_child){
+                        $is_child = false;
+                        if($hide_cate){
+                            $value['current']   =   true;
+                            $va['current']      =   true;
+                        }else{
+                            $value['current'] 	= 	false;
+                            $va['current']      =   false;
+                        }
+                    }else{
+                        $va['current']      =   false;
+                    }
+                }
+            }
+        }
+        $this->assign('nodes',      $cate);
+        $this->assign('cate_id',    $this->cate_id);
+
+        //获取面包屑信息
+        $nav = get_parent_category($cate_id);
+        $this->assign('rightNav',   $nav);
+
+        //获取回收站权限
+        $show_recycle = $this->checkRule('Admin/article/recycle');
+        $this->assign('show_recycle', IS_ROOT || $show_recycle);
+        //获取草稿箱权限
+        $this->assign('show_draftbox', C('OPEN_DRAFTBOX'));
+    }
 
 }
