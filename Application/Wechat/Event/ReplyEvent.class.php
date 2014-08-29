0dc6bbad48d0ade2cfdd04c130b14973e0c58bd6
tealun <tealun@gmail.com>
2014-08-29 23:32:55 +0800

    优化了微信回复图文信息的数据选择
    在图文信息的选择上，增加了一个有效期的检测，即 deadline字段，只要该字段数值处于1到当前时间戳内，说明该文章已经过期，将不再选取出来。

--- Application/Wechat/Event/ReplyEvent.class.php
+++ Application/Wechat/Event/ReplyEvent.class.php
@@ -41,7 +41,13 @@ class ReplyEvent {
       //TODO 对分类进行多层子分类查询，并对含多个子目录的分类从子分类中平均提取新闻条目
       case 'news' :
         $catIdarr = M('Category')->where(array('id'=>$rs['reply_id'],'pid'=>$rs['reply_id'],'_logic'=>'OR'))->getField('id',true);
-        $news = M('Document')->where(array('category_id'=>array('in',$catIdarr),'status'=>array('eq',1)))->order('create_time desc')->getField('id,title,description,cover_id,link_id',8);
+        $news = M('Document')->where(
+				        											array(
+				        											'category_id'=>array('in',$catIdarr), //在指定目录及其子目录下的文档
+				        											'status'=>array('eq',1), //状态为启用的
+				        											'deadline'=>array(	'NOT BETWEEN ',array(1,time())) //截止日期为0或大于现在时间的
+																	)
+																)->order('create_time desc')->getField('id,title,description,cover_id,link_id',8);
         $reply = !empty($news)?get_news_arr($news):get_text_arr("哎呀，仓库里空空如也，啥也没找到！>_<|||\n你有哆啦A梦的口袋么？");
         unset($catIdarr,$news);
         break;
