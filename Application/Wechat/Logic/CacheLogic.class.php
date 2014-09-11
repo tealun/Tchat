485fb2f8cac7de60ac336968df7d2ffceed8cc00
tealun <tealun@gmail.com>
2014-09-11 18:01:38 +0800

    优化了客户缓存数据格式的处理
    并增加了匹配列表缓存处理、图文缓存处理方法

--- Application/Wechat/Logic/CacheLogic.class.php
+++ Application/Wechat/Logic/CacheLogic.class.php
@@ -25,32 +25,56 @@ class CacheLogic{
    */
   public function cacheReply($openId,$keyword){
   	$data =  S($openId);
-
-   	 if(in_array($keyword, $data['needs']['keyword'])){//判断客户发送的关键词是否与缓存继续处理指令相同
-	   	 		var_dump($data);
-	   	 	//return FALSE;
-	   	 }else{
-			$action = str2arr($data['action']['controller']); //读取缓存中需要的处理控制器和类名
-		    $methed = $data['action']['methed'];//读取缓存中需要的处理方法名
-		    if(!empty($data['needs']['params'])){//判断有无需要传入的参数
-			   	 $action = A($action[0],$action[1]);
-				 //如果在缓存中指定了传入变量，则通过指定控制器方法传入指定变量获取回复
-			     $reply = call_user_func_array(array($action,$methed), array($data['needs']['params'],$keyword));
-		    }else{
-		    	//如果缓存中没有指定传入变量，则将客户发送的关键字传入到指定控制器的方法
-			    $reply = A($action[0],$action[1])->$methed($keyword);
-			    }
+	$keywords = $data['needs']['keyword'];
+	
+	/* 判断是否是数组，是就不变动，不是就将字符串转换为数组 */
+	$keywords = is_array($keywords)?$keywords:str2arr($keywords);
+				
+   	 if(in_array($keyword,$keywords)){//判断客户发送的关键词是否与缓存继续处理指令相同
+   	 
+		$action = str2arr($data['action']['controller']); //读取缓存中需要的处理控制器和类名
+	    $methed = $data['action']['methed'];//读取缓存中需要的处理方法名
+	    
+	    if(!empty($data['needs']['params'])){//判断有无需要传入的参数
+		   	 $action = A($action[0],$action[1]);
+			 //如果在缓存中指定了传入变量，则通过指定控制器方法传入指定变量获取回复
+		     $reply = call_user_func_array(array($action,$methed), array($openId,$keyword,$data['needs']['params']));
+	    }else{
+	    	//如果缓存中没有指定传入变量，则将客户发送的关键字传入到指定控制器的方法
+		    $reply = A($action[0],$action[1])->$methed($openId,$keyword);
+		    }
+		
+		return $reply;
+	  }else{
+		return FALSE;
 		 }
-    return $reply;
   }
+  
+  /**
+   * 回复缓存的关键词组列表
+   * @param string $openId 客户openId
+   * @param int $id 客户发送的关键词组ID 
+   */
+  public function listCache($openId,$keyword){
+  	$data = S($openId);//获取客户缓存
+	/* 查询对应的回复信息 */
+    $map['id']  = $data['listCache'][$keyword];
+	$rs = M('Tchat_keyword_group')->where($map)->find();
+	/* 回复内容 */
+    $reply = A('Reply','Event')->wechatReply($rs);
+
+	return $reply;
+  }
+  
   
   /**
    * 回复缓存的图文数据
    * @param array $news 缓存的图文信息数据
    */
-  public function newsCache($news){
-  		var_dump($news);
-  	return get_news_arr($news);
+  public function newsCache($openId,$keyword=''){
+	$cache = S($openId);
+	$news = $cache['news'];
+  	return $reply = get_news_arr($news);
   }
   
 }
