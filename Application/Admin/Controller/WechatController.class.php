4461226c162b4236897867547938d91466a00f64
tealun <tealun@gmail.com>
2014-11-16 23:30:09 +0800

    提供一个可以查看和复制所需内容ID和URL的方法
    还在编辑

--- Application/Admin/Controller/WechatController.class.php
+++ Application/Admin/Controller/WechatController.class.php
@@ -19,6 +19,34 @@ class WechatController extends AdminController {
 		$this ->assign('meta_title','微信管理');
 		$this->display();
 	}
+	
+	/**
+	 * 内容导航
+	 * 为添加分类及文章时查找分类和文章的ID及链接提供导航
+	 */
+	public function contentGuide($segment=''){
+		$map = array(
+			'status' => 1
+		);
 
-
+		switch ($segment) {
+			case 'category':
+				$guideList = D('Category')->where($map)->select();
+				break;
+			case 'article':
+				$Document   =   D('Document');
+				        //只查询pid为0的文章
+		        $map['pid'] = 0;
+		        $guideList  = $this->lists($Document,$map,'update_time desc');
+		        int_to_string($guideList );
+				
+				break;
+			default:
+				return FALSE;
+				break;
+		}
+		$this->assign('segment',$segment);
+		$this->assign('guideList',$guideList);
+		$this->display('contentguide');
+	}
 }
