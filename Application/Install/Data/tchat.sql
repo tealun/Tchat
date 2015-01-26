306334b4212b1ef3a00cbb58fbc9b4bc94d78493
tealun <tealun@gmail.com>
2015-01-26 17:42:57 +0800

    新增加了客户发送消息查无回复时的管理事件

--- Application/Install/Data/tchat.sql
+++ Application/Install/Data/tchat.sql
@@ -781,6 +781,8 @@ CREATE TABLE `onethink_tchat_events` (
 INSERT INTO `onethink_tchat_events` VALUES ('1','客户关注','subscribe','text','2','1','当客户新关注公众帐号时的回复内容设置');
 INSERT INTO `onethink_tchat_events` VALUES ('2','扫描二维码','SCAN','news','21','1','已关注客户扫描带二维码事件时的回复内容。<br /><strong>注意：</strong>如果客户是通过扫描二维码关注帐号时，回复内容为“关注事件”中设置的内容，如果是已经关注的客户扫描，则回复本设置中的内容。');
 INSERT INTO `onethink_tchat_events` VALUES ('3','上报地理位置','LOCATION','','','-1','客户上报地理位置事件的回复内容。');
+INSERT INTO `onethink_tchat_events` VALUES ('4','关键词没有结果','KeywordNoMatch','','','1','客户在发送关键词后没有查找到关键词对应内容时的回复。');
+INSERT INTO `onethink_tchat_events` VALUES ('5','分类中没有文章','No','','','1','查找某个分类下的内容却没有任何正常状态下的文章时的回复。');
 
 -- -----------------------------
 -- Table structure for `onethink_tchat_keyword`
