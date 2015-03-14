54d1930e5ca3b63c0201ce9c667949ec3914fd9f
tealun <tealun@gmail.com>
2015-03-14 17:46:30 +0800

    前台控制插件完善homeFeature钩子

--- Addons/HomeControl/HomeControlAddon.class.php
+++ Addons/HomeControl/HomeControlAddon.class.php
@@ -51,15 +51,22 @@ use Common\Controller\Addon;
 		 * 此前台控制首页中特色栏目导航
 		 */
 		public function homeFeature(){
+	
+			//读取缓存中的特色内容
 			$feature = F('Tchat/homeFeature');
 			
 			//赋值特色内容条目
-			if(is_null($feature['items'])){
+			if(empty($feature['items'])){
 				$itemsCount = 0;
 			}else{
 				$itemsCount = count($feature['items']);
+				foreach ($feature['items'] as $key => $value) {
+					$feature['items'][$key]=$this->findItemInfo($value['type'],$value['id']);
+				}
+				$itemsCount = count($feature['items']);
 				$this->assign('featureItems',$feature['items']);
 				$this->assign('featureItemsCount',$itemsCount);
+				
 			}
 			
 			//赋值特色内容板块标题
@@ -71,8 +78,6 @@ use Common\Controller\Addon;
 			if($feature['moreLinkUrl']){
 				$this->assign('featureMoreLinkUrl',$feature['moreLinkUrl']);
 			}
-			
-			$this->assign('featureStyle',$feature['style']);
 			
 			$this->display('feature');
 		}
@@ -89,6 +94,85 @@ use Common\Controller\Addon;
 			$afterArticle = F('Tchat/homeAfterArticle');
 			$this->assign('addons_afterArticle',$afterArticle);
 			$this->display('afterArticle');	
+		}
+
+		private function findItemInfo($segment=' ',$id=' '){
+					
+		switch ($segment) {
+				case 'category': //内容分类类型
+					$category = M('category')->find($id);
+				
+					$info['segment'] = $segment;
+					$info['id']=$id;
+					$info['url'] = "/Home/category/id/".$id.".html";
+					$info['image'] = get_cover($category['icon'],'path');
+					$info['title']=$category['title'];
+					$info['alt']=$category['title'];
+					$info['caption']=$category['description'];
+					
+					unset($category);
+					break;
+					
+				case 'article'||'product'://文章及产品类型
+					
+					$article = D('Document')->detail($id); //获取条目内容详情
+					
+					$info['segment'] = $segment;
+					$info['id']=$id;
+					$info['url'] = "/Home/article/detail?id=".$id;
+					$info['image'] = get_cover($article['cover_id'],'path');
+					$info['alt']=$article['title'];
+					$info['caption']=$article['title'];
+					
+					unset($article); //删除article的变量
+					break;
+					
+				case 'activity'://活动类型
+					$activity = D('Tchat_activity')->detail($id); //获取条目内容详情
+					
+					$info['segment'] = $segment;
+					$info['id']=$id;
+					$info['url'] = "/Home/activity/detail?id=".$id;
+					$info['image'] = get_cover($activity['cover_id'],'path');
+					$info['alt']=$activity['title'];
+					$info['caption']=$activity['title'];
+					
+					unset($activity); //删除article的变量
+					break;
+					
+ 				case 'plan'://套餐类型
+					$plan = D('Tchat_plan')->info($id); //获取条目内容详情
+					
+					$info['segment'] = $segment;
+					$info['id']=$id;
+					$info['url'] = "/Home/activity/detail?id=".$id;
+					$info['image'] = get_cover($plan['cover_id'],'path');
+					$info['alt']=$plan['title'];
+					$info['caption']=$plan['title'];
+					
+					unset($plan);
+					break;
+										
+				case 'topic'://专题类型
+					$topic = D('Tchat_topic')->info($id); //获取条目内容详情
+					
+					$info['segment'] = $segment;
+					$info['id']=$id;
+					$info['url'] = "/Home/topic/detail?id=".$id;
+					$info['image'] = get_cover($topic['cover_id'],'path');
+					$info['alt']=$topic['title'];
+					$info['caption']=$topic['title'];
+					
+					unset($topic);
+					break;
+					
+				default:
+					
+					break;
+			}
+
+
+					return $info;//返回整合后的条目内容
 		}
 
     }
